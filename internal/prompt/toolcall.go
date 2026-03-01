677e836805837438adce8c69cc91565eaeef5f13
gaoxiao <gaoxiao6331@163.com>
2026-03-02 00:04:20 +0800

    feat: add system prompts for various agent types

---
+++ internal/prompt/toolcall.go
@@ -0,0 +1,6 @@
+package prompt
+
+const (
+	ToolcallSystemPrompt   = "You are an agent that can execute tool calls"
+	ToolcallNextStepPrompt = "If you want to stop interaction, use `terminate` tool/function call."
+)
